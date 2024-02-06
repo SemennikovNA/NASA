ba772a191e9f14db1190b69aa5eb5a348d6719c2
Nikita <semennikovna@yandex.ru>
2024-02-06 19:09:55 +0600

    Create and setup tabBar controller

---
+++ NASA/Controllers/DayPictureViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DayPictureViewController.swift
+//  NASA
+//
+//  Created by Nikita on 06.02.2024.
+//
+
+import UIKit
+
+class DayPictureViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+    }
+}
---
+++ NASA/Controllers/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  NASA
+//
+//  Created by Nikita on 06.02.2024.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+    }
+}
